0308d0e4488426429ef59a201bc28fd3a9ac5558
srueda99 <ruedamarinsebastian@gmail.com>
2022-01-12 11:33:08 -0500

    Funciones

---
+++ functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS
+    #define FUNCTIONS
+
+        void caps(char word[20]);
+        void low(char word[20]);
+        void mix(char word[20]);
+        void change(char word[20], char old[5], char newl[5]);
+        void terminal();
+        void validarIP(char ip[15]);
+        void ascii();
+        int random();
+        void concatenar(char p1[20], char p2[20]);
+
+    #include  <functions.c>
+#endif
